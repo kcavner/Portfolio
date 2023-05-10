173740133caa096a396381054332fb3a3e5600b4
unknown <kcavner@hotmail.com>
2023-05-10 16:35:59 -0500

    fixed images again

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,51 +1,67 @@
 import React from 'react';
-import PortfolioCard from './portfolioCard';
+
+import abcdojo from "../assets/abcdojo.jpg";
+import forum from "../assets/forum.png";
+import movie from "../assets/movie.jpg";
+import weather from "../assets/weather.jpg";
+import raycast from "../assets/raycast.jpg";
+import workday from "../assets/workday.jpg";
+import githubPic from "../assets/github.png"
+
+
+
 
 
 const portfolioItems = [
     {
       id: 1,
-      imageSrc:'../assets/forum.png',
+      imageSrc: forum,
       github: 'https://github.com/amsudekum/Forum-Project',
     },
     {
       id: 2,
-      imageSrc: '../assets/weather.JPG',
+      imageSrc: weather,
       github: 'https://github.com/kcavner/weather-dashboard',
     },
     {
       id: 3,
-      imageSrc: '../assets/movie.JPG',
+      imageSrc: movie,
       github: 'https://github.com/jboyce313/movie-game',
     },
     {
       id: 4,
-      imageSrc: '../assets/raycast.JPG',
+      imageSrc: raycast,
       github: 'https://github.com/kcavner/raycast-visualizer',
     },
     {
       id: 5,
-      imageSrc: '../assets/abcdojo.JPG',
+      imageSrc: abcdojo,
       github: 'https://kcavner.github.io/abc-dojo/',
     },
     {
       id: 6,
-      imageSrc: '../assets/workday.jpg',
+      imageSrc: workday,
       github: 'https://github.com/kcavner/work-day-scheduler',
     },
   ];
 
+
 function Portfolio(){
+
+  const openGithub = (github) => {
+    window.open(github, '_blank');
+  };
+
     return (
         <div className='portfolioName'>My Projects
         <div className='portfolioPage'>
              {portfolioItems.map((item) => (
-        <PortfolioCard
-          key={item.id}
-          imageSrc={item.imageSrc}
-          github={item.github}
-          
-        />
+              <div className='portfolioCard'>
+          <img src={item.imageSrc} alt="portfolio item" onClick={() => openGithub(item.github)} style={{position:"absolute",width: "20vw", height: "15vw" }} onMouseOver={(e) =>{e.currentTarget.style.opacity = .4;}} onMouseOut={(e) => {
+        e.currentTarget.style.opacity = 1;
+      }} />
+        <img src={githubPic}alt="portfolio item" style={{width: "20vw", height: "15vw" }}/>
+      </div>
       ))}
            
         </div>
